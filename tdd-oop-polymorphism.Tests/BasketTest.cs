018b4b1cce31ddf52a416a25bbe2054e82a56a15
uerbzr <nigel.sibbert@gmail.com>
2023-05-15 15:19:23 +0100

    updated

---
+++ tdd-oop-polymorphism.Tests/BasketTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using tdd_oop_polymorphism.CSharp.Main;
+
+namespace tdd_oop_polymorphism.Tests
+{
+    class BasketTest
+    {
+        [Test]
+        public void shouldCalculateTotal()
+        {
+            Basket basket = new Basket();
+            basket.add(new Game("Morrowind", 12));
+            basket.add(new Book("Darkly Dreaming Dexter", 6));
+            basket.add(new Drink("Cappucino", 2));
+
+            Assert.AreEqual(20, basket.getTotal());
+        }
+
+        [Test]
+        public void shouldFindExistingProduct()
+        {
+            Basket basket = new Basket();
+            basket.add(new Game("Morrowind", 12));
+            basket.add(new Book("Darkly Dreaming Dexter", 6));
+            basket.add(new Drink("Cappucino", 2));
+
+            Assert.IsTrue(basket.isInBasket("Morrowind"));
+        }
+
+        [Test]
+        public void shouldNotFindNonExistingProduct()
+        {
+            Basket basket = new Basket();
+            basket.add(new Game("Morrowind", 12));
+            basket.add(new Book("Darkly Dreaming Dexter", 6));
+            basket.add(new Drink("Cappucino", 2));
+
+            Assert.IsFalse(basket.isInBasket("Skyrim"));
+        }
+    }
+}
